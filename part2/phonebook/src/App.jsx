e43e6a0db8ae7afdc2f9e8cd765276a4b92f35bc
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-01-05 15:39:28 +0000

    exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import axios from "axios";
+
 
 const Person = ({person}) => {
     return (
@@ -43,12 +45,18 @@ const Persons = ({persons, filter}) => {
 }
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-123456', id: 0},
-        {name: 'Ada Lovelace', number: '39-44-5323523', id: 1},
-        {name: 'Dan Abramov', number: '12-43-234345', id: 2},
-        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 3}
-    ])
+    const [persons, setPersons] = useState([])
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }, [])
+    console.log('render', persons.length, 'notes')
+
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filter, setNewFilter] = useState('')
